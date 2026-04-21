8aec94511f38de2fc45fd30291569b089b8fd7e5
PaulMbutu <pmbutum24@gmail.com>
2026-04-22 00:39:12 +0300

    Added home components

--- JS type/Ecommerce type/digital_shop/app/page.tsx
+++ JS type/Ecommerce type/digital_shop/app/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Hero from '@/components/home/Hero'
+import CategorySection from '@/components/home/CategorySection'
+import ProductSection from '@/components/home/ProductSection'
 const HomePage = () => {
   return (
-    <div>
-      Hello world!
-    </div>
+          <>
+            <Hero/>
+            <CategorySection/>
+            <ProductSection title='Featured products'/>
+          </>
   )
 }
 
