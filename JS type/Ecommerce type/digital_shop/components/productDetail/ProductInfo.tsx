ce7c25d705248b1f65c62eafa4570b1861fb483d
PaulMbutu <pmbutum24@gmail.com>
2026-04-23 01:23:46 +0300

    Sus commit, one drive deletions masacre.

--- JS type/Ecommerce type/digital_shop/components/productDetail/ProductInfo.tsx
+++ JS type/Ecommerce type/digital_shop/components/productDetail/ProductInfo.tsx
@@ -7,7 +7,7 @@ const ProductInfo = () => {
     <div className="bg-gray-50 padding-x py-10 flex items-start flex-wrap gap-12 main-max-width padding-x mx-auto">
       {/* Product Image */}
     
-      <div className="w-87.5 h-100 relative overflow-hidden rounded-lg shadow-sm border border-gray-200">
+      <div className="w-[350px] h-[400px] relative overflow-hidden rounded-lg shadow-sm border border-gray-200">
         <Image
           src="/gaming_pad.jpg"
           alt="gaming"
@@ -17,9 +17,9 @@ const ProductInfo = () => {
       </div>
 
       {/* Product Info */}
-      <div className="flex flex-1 flex-col gap-6 max-w-125 max-md:w-full">
+      <div className="flex flex-1 flex-col gap-6 max-w-[500px] max-md:w-full">
         <div className="flex flex-col gap-3">
-          <h1 className="text-3xl font-bold">Gaming Pad</h1>
+          <h1 className="text-3xl font-bold">AppleSmart Watch</h1>
           <h3 className="text-2xl font-semibold text-black">$200</h3>
           
         </div>
@@ -29,7 +29,8 @@ const ProductInfo = () => {
           <h3 className="font-medium text-lg mb-3">Details</h3>
           <p className="text-gray-600 text-justify leading-6 text-[14px] max-md:text-[12px]">
             {/* {product.description} */}
-              A gamming pad used by XBox  gaming consoles.
+            Just as a book is judged by its cover, the first thing you notice when you pick up a modern smartphone is the display. Advanced technologies allow for edge-to-edge screens with minimal cutouts, and Apple consistently excels in this area. 
+            The 6.7-inch Retina panels with ProMotion from last year received widespread praise for their exceptional quality.
           </p>
         </div>
 
